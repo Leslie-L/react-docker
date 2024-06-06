e8efd1e255b13c90725cdbba550d1cd9374ef262
Leslie-L <leslie.centeno@galileo.edu>
2024-06-06 11:16:06 -0600

    cleaning folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App(){
   return (
       <div className="h-screen w-full bg-black">
-        <h1 className="text-white font-bold">Hello Docker!!! 🔍</h1>
+        <h1 className="text-gray-400 font-bold">Hello Docker!!! 🔍</h1>
       </div> 
   )
 }
